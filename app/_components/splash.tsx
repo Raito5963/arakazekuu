cfd647ca24fdeefece9aa1aa2b70255ff811b8ed
Raito5963 <181489458+Raito5963@users.noreply.github.com>
2025-03-28 16:22:25 +0000

    スプラッシュ画面を動画からアニメーションに変更し、フェードアウトのタイミングを調整

--- app/_components/splash.tsx
+++ app/_components/splash.tsx
@@ -1,32 +1,23 @@
 "use client";
-import Image from "next/image";
 import "./../style/globals.css";
 
 const Splash = () => {
-    return (
-        <div style={{ width: "100vw", height: "100vh", overflow: "hidden", backgroundColor: "transparent" }}>
-            <video
-                autoPlay
-                muted
-                playsInline
-                // 古いSafari向け
-                webkit-playsinline="true"
-                style={{
-                    objectFit: 'cover',
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100vw', 
-                    height: '100vh',
-                    zIndex: 9999, // 他のコンテンツより前面に表示
-                    backgroundColor: 'transparent',
-                }}
-            >
-                <source src="/cloud_and_water_transparent.mov" type="video/quicktime" />
-                <source src="/output.webm" type="video/webm" />
-            </video>
-        </div>
-    );
+  return (
+    <div 
+      className="move" 
+      style={{ 
+        width: "100vw", 
+        height: "100vh", 
+        overflow: "hidden", 
+        backgroundColor: "transparent" 
+      }}
+    >
+      {/* 波のアニメーション */}
+      <div className="wave-container">
+        <div className="wave"><div className="sample"></div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Splash;
